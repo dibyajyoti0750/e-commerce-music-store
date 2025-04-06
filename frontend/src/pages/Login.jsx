0cb2cb468e9780f6a17b05c6dff405a493878a54
Dibyajyoti Pramanick <dibyajyotipramanick0750@gmail.com>
2025-04-06 07:55:29 +0530

    Create login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,8 +5,15 @@ const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
   const [showPassword, setShowPassword] = useState(false);
 
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
       {/* Heading */}
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
